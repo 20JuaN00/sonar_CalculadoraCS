e94dfa0ff0576b97eba28a30ea28ed15672199ee
20JuaN00 <20juandiego00@gmail.com>
2025-11-18 18:49:56 -0500

    Refactor

--- LaMalaCalculadora/Program.cs
+++ LaMalaCalculadora/Program.cs
@@ -1,233 +1,250 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Threading;
 
-    using System;
-    using System.Collections;
-    using System.Collections.Generic;
-    using System.Globalization;
-    using System.IO;
-    using System.Net;
-    using System.Text;
-    using System.Threading;
-    using Xceed.Document.NET;
 
-    namespace BadCalcVeryBad
+namespace BadCalcVeryBad
+{
+    public class DatosGlobales
     {
+        public readonly ArrayList Historial = new ArrayList();
+        public const int contador = 0;
+        public string Misc { get; set; }
+    }
 
-
-        public class DatosGlobales
+    public static class Calculadora
+    {
+        public static double Calcular(string a, string b, string operador)
         {
-            public readonly ArrayList Historial = new ArrayList();
-            public const int contador = 0;
-            public string Misc { get; set; }
-        }
+            double NumA = ConvertirTexto(a);
+            double NumB = ConvertirTexto(b);
 
-        public static class Calculadora
-        {
-        
-
-            public static double Calcular(string a, string b, string operador)
+            switch (operador)
             {
-                double NumA = ConvertirTexto(a);
-                double NumB = ConvertirTexto(b);
+                case "+":
+                    return NumA + NumB;
 
-            
-
-                switch (operador)
-                {
-                    case "+":
-                        return NumA + NumB;
-
-                    case "-":
-                        return NumA + NumB;
+                case "-":
+                    return NumA - NumB;
 
                 case "*":
-                        return NumA + NumB;
+                    return NumA * NumB;
 
                 case "/":
-                        if (Math.Abs(NumB) < 0.0000001)  
-                        {
-                            throw new DivideByZeroException("No se puede dividir entre cero");
-                        }
-                        return NumA + NumB;
-
-
+                    if (Math.Abs(NumB) < 0.0000001)
+                    {
+                        throw new DivideByZeroException("No se puede dividir entre cero");
+                    }
+                    return NumA / NumB;
 
                 case "^":
-                        double z = 1;
-                        int i = (int)NumB;
-                        while (i > 0) { z *= NumA; i--; }
-                        return z;
-
-                    case "%":
-                        return NumA % NumB;
-
-                    default:
-                        Console.WriteLine("Numero equivocado intente de nuevo");
-                        return 0;
-                }
-            
-            
-            
-
-                static double ConvertirTexto(string texto)
-                {
-                    try
+                    double z = 1;
+                    int i = (int)NumB;
+                    while (i > 0)
                     {
-                        return double.Parse(texto.Replace(',', '.'), CultureInfo.InvariantCulture);
+                        z *= NumA;
+                        i--;
                     }
-                    catch (Exception ex)
-                    {
-                        Console.WriteLine(ex.ToString());
-                        return 0;
-                    }
-                }
-            
-        
+                    return z;
+
+                case "%":
+                    return NumA % NumB;
+
+                default:
+                    Console.WriteLine("Numero equivocado intente de nuevo");
+                    return 0;
             }
         }
 
-   
-
-        public static class Program
+        static double ConvertirTexto(string texto)
         {
-        
-            private static readonly DatosGlobales globals = new DatosGlobales();   
-
-            //Inicia!!!!!!!!!!!!!!!!!!
-            public static void Main(string[] args)
+            try
             {
-                try
-                {
-                    string operacion = "";
-                    do
-                    {
-                        Console.WriteLine("BAD CALC - worst practices edition");
-                        Console.WriteLine("1) add  2) sub  3) mul  4) div  5) pow  6) mod  7) sqrt  8) hist 0) exit");
-                        Console.Write("opt: ");
-                        var o = Console.ReadLine();
-                        string a = "0", b = "0";
-                        double res = 0;
-
-                        if (int.Parse(o) < 0 || int.Parse(o) > 8)
-                        {
-                            Console.WriteLine("Opcion invalida, intente de nuevo");
-                            continue;
-                        }
-
-                        operacion = o switch
-                        {
-                            "1" => "+", //Accion
-                            "2" => "-",
-                            "3" => "*",
-                            "4" => "/",
-                            "5" => "^",
-                            "6" => "%",
-                            "7" => "sqrt",
-                            "8" => "hist",
-                            "0" => "exit",
-                            _ => ""  // default
-                        };
-
-
-                        if (operacion == "exit")
-                        {
-                            Console.WriteLine("Saliendo...");
-                            return;
-                        }
-
-                        else if (operacion == "hist")
-                        {
-                            //Historial
-                            Console.WriteLine("\n=== HISTORY ===");
-                            foreach (var item in globals.Misc) Console.WriteLine(item);
-                        continue;
-
-
-
-                        }
-
-                        Console.Write("a: ");
-                        a = Console.ReadLine();
-
-                        if (operacion != "sqrt" )
-                        {
-                        
-                            Console.Write("b: ");
-                            b = Console.ReadLine();
-                        }
-
-                         if (operacion == "sqrt")
-                         {
-                        
-                            double A = TryParse(a);
-                            if (A < 0) res = TrySqrt(Math.Abs(A)); else res = TrySqrt(A);
-
-                        
-
-                         }
-
-
-                    
-
-                        else
-                        {
-
-                            
-
-                            res = Calculadora.Calcular(a, b, operacion);
-                        
-
-                        }
-                        Console.WriteLine("= " + res.ToString(CultureInfo.InvariantCulture));
-
-                        //Guardar en historial
-                        try
-                        {
-                            var line = a + "|" + b + "|" + operacion + "|" + res.ToString("0.###############", CultureInfo.InvariantCulture);
-                            globals.Historial.Add(line);
-                            globals.Misc = line;
-                            File.AppendAllText("history.txt", line + Environment.NewLine);
-                        }
-                        catch (Exception ex)
-                        {
-                            Console.WriteLine(ex.ToString());
-                        }
-
-
-
-                    } while (operacion != "exit");
-                }
-                catch (Exception ex)
-                {
-                    Console.WriteLine("Ingrese un dato correcto ");    
-                }
-            
-            
-
-        
-            
-
-           
+                return double.Parse(texto.Replace(',', '.'), CultureInfo.InvariantCulture);
             }
-            //Acaba!!!!!!!!!!!!!!!!!!
-
-
-            static double TryParse(string s)
+            catch (Exception ex)
             {
-                try { return double.Parse(s.Replace(',', '.'), CultureInfo.InvariantCulture); } catch { return 0; }
-            }
-
-            static double TrySqrt(double v)
-            {
-                double g = v;
-
-                int k = 0;
-                while (Math.Abs(g * g - v) > 0.0001 && k < 100000)
-                {
-                    g = (g + v / g) / 2.0;
-                    k++;
-                
-                }
-                return g;
+                Console.WriteLine(ex.ToString());
+                return 0;
             }
         }
     }
+
+    public static class Program
+    {
+        private static readonly DatosGlobales globals = new DatosGlobales();
+
+        //Inicia!!!!!!!!!!!!!!!!!!
+        public static void Main(string[] args)
+        {
+            try
+            {
+                string operacion = "";
+                do
+                {
+                    operacion = ProcesarCalculo();
+                } while (operacion != "exit");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Ingrese un dato correcto");
+            }
+        }
+        //Acaba!!!!!!!!!!!!!!!!!!
+
+        private static string ProcesarCalculo()
+        {
+            MostrarMenu();
+            string operacion = ObtenerOperacion();
+
+            if (operacion == "exit")
+            {
+                Console.WriteLine("Saliendo...");
+                return "exit";
+            }
+
+            if (operacion == "hist")
+            {
+                MostrarHistorial();
+                return "hist";
+            }
+
+            RealizarCalculo(operacion);
+            return operacion;
+        }
+
+        private static void MostrarMenu()
+        {
+            Console.WriteLine("BAD CALC - worst practices edition");
+            Console.WriteLine("1) add  2) sub  3) mul  4) div  5) pow  6) mod  7) sqrt  8) hist 0) exit");
+            Console.Write("opt: ");
+        }
+
+        private static string ObtenerOperacion()
+        {
+            var o = Console.ReadLine();
+            int numeroOpcion = int.Parse(o);
+
+            if (numeroOpcion < 0 || numeroOpcion > 8)
+            {
+                Console.WriteLine("Opcion invalida, intente de nuevo");
+                return "";
+            }
+
+            string operacion = o switch
+            {
+                "1" => "+", //Accion
+                "2" => "-",
+                "3" => "*",
+                "4" => "/",
+                "5" => "^",
+                "6" => "%",
+                "7" => "sqrt",
+                "8" => "hist",
+                "0" => "exit",
+                _ => ""  // default
+            };
+
+            return operacion;
+        }
+
+        private static void MostrarHistorial()
+        {
+            //Historial
+            Console.WriteLine("\n=== HISTORY ===");
+            foreach (var item in globals.Historial)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        private static void RealizarCalculo(string operacion)
+        {
+            Console.Write("a: ");
+            string a = Console.ReadLine();
+            string b = "0";
+
+            if (operacion != "sqrt")
+            {
+                Console.Write("b: ");
+                b = Console.ReadLine();
+            }
+
+            double res = CalcularResultado(a, b, operacion);
+            Console.WriteLine("= " + res.ToString(CultureInfo.InvariantCulture));
+            GuardarEnHistorial(a, b, operacion, res);
+        }
+
+        private static double CalcularResultado(string a, string b, string operacion)
+        {
+            if (operacion == "sqrt")
+            {
+                return CalcularRaizCuadrada(a);
+            }
+            else
+            {
+                return Calculadora.Calcular(a, b, operacion);
+            }
+        }
+
+        private static double CalcularRaizCuadrada(string a)
+        {
+            double A = TryParse(a);
+            if (A < 0)
+            {
+                return TrySqrt(Math.Abs(A));
+            }
+            else
+            {
+                return TrySqrt(A);
+            }
+        }
+
+        private static void GuardarEnHistorial(string a, string b, string operacion, double res)
+        {
+            //Guardar en historial
+            try
+            {
+                var line = a + "|" + b + "|" + operacion + "|" + res.ToString("0.###############", CultureInfo.InvariantCulture);
+                globals.Historial.Add(line);
+                globals.Misc = line;
+                File.AppendAllText("history.txt", line + Environment.NewLine);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+
+        static double TryParse(string s)
+        {
+            try
+            {
+                return double.Parse(s.Replace(',', '.'), CultureInfo.InvariantCulture);
+            }
+            catch
+            {
+                return 0;
+            }
+        }
+
+        static double TrySqrt(double v)
+        {
+            double g = v;
+            int k = 0;
+
+            while (Math.Abs(g * g - v) > 0.0001 && k < 100000)
+            {
+                g = (g + v / g) / 2.0;
+                k++;
+            }
+
+            return g;
+        }
+    }
+}
